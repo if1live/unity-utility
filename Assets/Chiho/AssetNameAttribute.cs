cd76cc3275445630495077aaa18a6ede89e0c636
if1live <libsora25@gmail.com>
2016-10-09 14:02:03 +0900

    AssetNameAttribute + sample

---
+++ Assets/Chiho/AssetNameAttribute.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Reflection;
+#endif
+
+namespace Assets.Chiho {
+    public class AssetNameAttribute : Attribute {
+        string v;
+
+        public AssetNameAttribute(string name) {
+            this.v = name;
+        }
+
+#if UNITY_EDITOR
+        public UnityEngine.Object FindAsset(Type t) {
+            var assetPath = FindAssetPath(v);
+            if (assetPath != null) {
+                return AssetDatabase.LoadAssetAtPath(assetPath, t);
+            } else {
+                Debug.LogFormat("Cannot find {0}", v);
+                return null;
+            }
+        }
+
+        public static void ConnectMemberAssets(object obj) {
+            var t = obj.GetType();
+            var fields = t.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var f in fields) {
+                var attrs = f.GetCustomAttributes(typeof(AssetNameAttribute), false);
+                if (attrs.Length == 0) {
+                    continue;
+                }
+
+                var attr = attrs[0] as AssetNameAttribute;
+                var prev = f.GetValue(obj);
+                var x = prev.GetType();
+                if (prev == null || prev.ToString() == "null") {
+                    var asset = attr.FindAsset(f.FieldType);
+                    f.SetValue(obj, asset);
+                }
+            }
+        }
+
+        static string FindAssetPath(string name) {
+            var filename = Path.GetFileNameWithoutExtension(name);
+            var founds = AssetDatabase.FindAssets(filename);
+            foreach (var guid in founds) {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                if (assetPath.EndsWith(name)) {
+                    return assetPath;
+                }
+            }
+            return null;
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/AssetNameConnectSample.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Chiho;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class AssetNameConnectSample : MonoBehaviour {
+        [AssetName("Click_Heavy_00.mp3")]
+        public AudioClip clickA;
+
+        [AssetName("Click_Mechanical_00.mp3")]
+        public AudioClip clickB;
+
+        [AssetName("twitter_dark.png")]
+        public Texture2D icon;
+
+        [AssetName("icons_set_black.png")]
+        public Texture2D iconSet;
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(AssetNameConnectSample))]
+    public class AssetNameConnectorEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            var script = (AssetNameConnectSample)target;
+            if(GUILayout.Button("Connect")) {
+                AssetNameAttribute.ConnectMemberAssets(script);
+            }
+        }
+    }
+#endif
+}
