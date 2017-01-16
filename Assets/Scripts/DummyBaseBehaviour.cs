1372655bff36cccc50535374d1e8b190ebcf12d4
if1live <libsora25@gmail.com>
2017-01-16 23:51:48 +0900

    inspector example

---
+++ Assets/Scripts/DummyBaseBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Vexe.Runtime.Types;
+
+namespace Assets.Scripts {
+    [DefineCategory("foo", Pattern = "^foo_")]
+    [DefineCategory("bar", Pattern = "^bar_")]
+    class DummyBaseBehaviour : BaseBehaviour {
+        [Show]
+        void PrintLog() {
+            Debug.Log("method invoked - base behaviour");
+        }
+
+        [Show]
+        void PrintDebugLog() {
+            dLog("debug log");
+            dLogFormat("{0} {1} {2}", "debug", "log", "+ format");
+        }
+
+        public int foo_int = 0;
+        public float foo_float = 0;
+
+        public int bar_int = 0;
+        public float bar_float = 0;
+    }
+}
---
+++ Assets/Scripts/DummyMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Assets.Scripts {
+    class DummyMonoBehaviour : MonoBehaviour {
+        public void PrintLog() {
+            Debug.Log("method invoked - mono behaviour");
+        }
+
+        [Header("foo")]
+        public int foo_int = 0;
+        public float foo_float = 0;
+
+        [Header("bar")]
+        public int bar_int = 0;
+        public float bar_float = 0;
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(DummyMonoBehaviour))]
+    class DummyMonoBehaviourEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            var script = (DummyMonoBehaviour)target;
+            if(GUILayout.Button("InvokeMethod")) {
+                script.PrintLog();
+            }
+        }
+    }
+#endif
+}
